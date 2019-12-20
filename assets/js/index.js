9c3bfaff77ddbce3925ec06f43b7c16b73ff9648
Ozymandias <raymondlee@kaffeinatenc.com>
2019-12-19 20:41:40 -0500

    Added authentication url to sign in widget

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,17 +19,10 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
         // Hide the loader.
         document.getElementById('loader').style.display = 'none';
       },
-      writeUserData: function(userId, name, email, imageUrl) {
-        firebase.database().ref('users/' + userId).set({
-          username: name,
-          email: email,
-          profile_picture : imageUrl,
-          newUser : true
-      })}
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInSuccessUrl: "./authenticated.html",
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
