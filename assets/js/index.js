1d41033c986311441406568ac99089ecaf573533
travis-john <travis.wl.john@gmail.com>
2019-12-21 13:08:28 -0500

    NYT API functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,47 @@
 ///// FIREBASE CODE START /////
 
 // Initialize the FirebaseUI Widget using Firebase.
-// var ui = new firebaseui.auth.AuthUI(firebase.auth());
-//   ui.start('#firebaseui-auth-container', {
-//     signInOptions: [
-//       firebase.auth.EmailAuthProvider.PROVIDER_ID
-//     ],
-//     // Other config options...
-//   });
-//   var uiConfig = {
-//     callbacks: {
-//       signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-//         // User successfully signed in.
-//         // Return type determines whether we continue the redirect automatically
-//         // or whether we leave that to developer to handle.
-//         return true;
-//       },
-//       uiShown: function() {
-//         // The widget is rendered.
-//         // Hide the loader.
-//         document.getElementById('loader').style.display = 'none';
-//       },
-//       writeUserData: function(userId, name, email, imageUrl) {
-//         firebase.database().ref('users/' + userId).set({
-//           username: name,
-//           email: email,
-//           profile_picture : imageUrl,
-//           newUser : true
-//       })}
-//     },
-//     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-//     signInFlow: 'popup',
-//     signInSuccessUrl: '<url-to-redirect-to-on-success>',
-//     signInOptions: [
-//       // Leave the lines as is for the providers you want to offer your users.
-//       firebase.auth.EmailAuthProvider.PROVIDER_ID,
-//     ],
-//     // Terms of service url.
-//     tosUrl: '<your-tos-url>',
-//     // Privacy policy url.
-//     privacyPolicyUrl: '<your-privacy-policy-url>'
-//   };
-//   ui.start('#firebaseui-auth-container', uiConfig);
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Other config options...
+  });
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      },
+      writeUserData: function(userId, name, email, imageUrl) {
+        firebase.database().ref('users/' + userId).set({
+          username: name,
+          email: email,
+          profile_picture : imageUrl,
+          newUser : true
+      })}
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+  ui.start('#firebaseui-auth-container', uiConfig);
 
   /////  NYT API CODE START /////
   var cryptoTrigger = $('.crypto-trigger'),
